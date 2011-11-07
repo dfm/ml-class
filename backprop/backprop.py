6e946d3937300415eb4d3f4239a18dde76bba0e2
Dan F-M <dan@danfm.ca>
2011-11-06 19:08:03 -0500

    added more docstrings

--- backprop/backprop.py
+++ backprop/backprop.py
@@ -9,7 +9,6 @@ from __future__ import division
 
 __all__ = ['Machine']
 
-import os
 import sys
 
 import numpy as np
@@ -53,9 +52,42 @@ class Machine(object):
         return r
 
     def add_module(self, module, args=(), kwargs={}):
+        """
+        Instatiate and append a new module to the machine
+
+        Parameters
+        ----------
+        module : LearningModule
+            This should be the class not an instance.
+
+        args : tuple
+            The arguments for module's constructor
+
+        kwargs : dict
+            The keyword arguments for module's constructor
+
+        """
         self._modules.append(module(*args, prev_module=self._modules[-1], **kwargs))
 
     def train_sample(self, sample, label, eta, decay):
+        """
+        Run a single stochastic gradient update for a given training sample
+
+        Parameters
+        ----------
+        sample : numpy.ndarray
+            The input vector
+
+        label : numpy.ndarray
+            The target output
+
+        eta : float
+            The learning rate
+
+        decay : float
+            The decay rate
+
+        """
         self._modules[0].set_current_sample(sample, label)
         self._modules[-1].do_fprop()
         self._modules[0].do_bprop()
@@ -65,6 +97,23 @@ class Machine(object):
                 m.w -= eta*(m.dw.T + decay*m.w)
 
     def training_sweep(self, eta, decay):
+        """
+        Perform a single stochastic gradient sweep over the full training set
+
+        Parameters
+        ----------
+        eta : float
+            The learning rate
+
+        decay : float
+            The decay rate
+
+        Returns
+        -------
+        loss : float
+            The mean loss over training samples
+
+        """
         x, y = self._data.training_set
         loss = 0.0
         for i in xrange(self._data.size_train):
@@ -73,6 +122,29 @@ class Machine(object):
         return loss/self._data.size_train
 
     def train(self, maxiter=100, miniter=5, tol=5.25e-5, eta=0.1, decay=0.0001):
+        """
+        Train the machine
+
+        Parameters
+        ----------
+        maxiter : int, optional
+            Maximum number of stochastic gradient sweeps (default: 100)
+
+        miniter : int, optional
+            Set a minimum number of sweeps to avoid premature convergence
+            (default: 5)
+
+        tol : float, optional
+            The convergence criterion for the relative change in the loss function
+            between training sweeps (default: 5.25e-5)
+
+        eta : float, optional
+            The learning rate (default: 0.1)
+
+        decay : float, optional
+            The decay rate (default: 0.0001)
+
+        """
         loss0 = 0.0
         msg = ""
         for i in xrange(maxiter):
@@ -96,6 +168,18 @@ class Machine(object):
                     %maxiter
 
     def test_sample(self, sample, label):
+        """
+        Test the classification of a given sample
+
+        Parameters
+        ----------
+        sample : numpy.ndarray
+            The input vector
+
+        label : numpy.ndarray
+            The target output
+
+        """
         # try to classify the sample
         self._modules[0].set_current_sample(sample, np.ones(label.shape, dtype=int))
         self._modules[-1].do_fprop()
@@ -110,6 +194,24 @@ class Machine(object):
         return self._modules[-1].x, error[0]
 
     def test(self, training_set=False):
+        """
+        Test the performance of the machine
+
+        Parameters
+        ----------
+        training_set : bool, optional
+            If True, test on the training set, otherwise, test on the test set
+            (default: False)
+
+        Returns
+        -------
+        loss : float
+            The mean loss over all samples in the dataset
+
+        err : float
+            The fractional error in the classification
+
+        """
         if training_set:
             x, y = self._data.training_set
             N = self._data.size_train
@@ -125,6 +227,15 @@ class Machine(object):
 
     @property
     def nparams(self):
+        """
+        Return the total number of parameters in this machine
+
+        Returns
+        -------
+        nparams : int
+            The total number of parameters in the machine
+
+        """
         n = 0
         for m in self._modules:
             if m.w is not None:
