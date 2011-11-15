104888849a360bc499d42081ae0e4775b00a6d41
Dan F-M <dan@danfm.ca>
2011-11-14 21:33:32 -0500

    updated experiments

--- main.py
+++ main.py
@@ -7,25 +7,35 @@
 
 from __future__ import division
 
-__all__ = ['']
-
 import numpy as np
-import pylab as pl
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pl
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 from mixtures import MixtureModel
 from dataset import ImageDataset
 from dataset._tile_helper import from_tiles
 
 def kmeans():
-    img   = ImageDataset('dataset/buildings.png')
-    model = MixtureModel(256, np.array(img.tiles, dtype=np.float64))
-    model.run_kmeans(tol=1e-4)
-    data = np.zeros(img.shape)
+    fig   = pl.figure(figsize=(10,11))
+    img   = ImageDataset('dataset/airplane.png')
 
-    from_tiles(model.means, model._kmeans_rs, data, (8,8))
+    grid = ImageGrid(fig, 111, nrows_ncols = (3, 2), axes_pad = 0.05, label_mode = 'L')
 
-    pl.imshow(data, cmap='gray')
-    pl.show()
+    grid[0].imshow(img.data, cmap='gray')
+
+
+    for i,k in enumerate([2,4,8,64,256]):
+        model = MixtureModel(k, np.array(img.tiles, dtype=np.float64))
+        model.run_kmeans(tol=1e-4)
+        data = np.zeros(img.shape)
+        from_tiles(model.means, model._kmeans_rs, data, (8,8))
+
+        ax = grid[i+1]
+        ax.imshow(data, cmap='gray')
+
+    pl.savefig('airplane.png')
 
 if __name__ == '__main__':
     kmeans()
